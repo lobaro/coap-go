c8aa884cd93b3a86bec5f6939b88e503d4f479f3
Tobias Kaupat <code@niondir.de>
2017-05-10 19:30:39 +0200

    Fix test

--- coap/client_test.go
+++ coap/client_test.go
@@ -20,7 +20,7 @@ func TestGetRequestFormat(t *testing.T) {
 	client := &Client{Transport: tr}
 	url := "http://dummy.faketld/"
 	_, err := client.Get(url) // Note: doesn't hit network
-	if err != nil {
+	if err != nil && err.Error() != "dummy impl" {
 		t.Error(err)
 	}
 
